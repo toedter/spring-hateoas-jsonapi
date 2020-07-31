5a4df68a7430d0d514b56957dd8fbcfc882aaa20
Kai Toedter <kai@toedter.com>
2020-07-31 09:45:20 +0200

    chore: add package-info.java for better JavaDoc

---
+++ lib/src/main/java/com/toedter/spring/hateoas/jsonapi/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * JSON:API-specific extensions, SPIs and Jackson customizations.
+ *
+ * @see <a href="https://jsonapi.org/">https://jsonapi.org/</a>
+ */
+@org.springframework.lang.NonNullApi
+package com.toedter.spring.hateoas.jsonapi;
