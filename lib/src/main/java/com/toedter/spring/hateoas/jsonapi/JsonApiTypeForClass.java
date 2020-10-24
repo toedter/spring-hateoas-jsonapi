86fa401f674654ee6a494e0cf9b9d48f5455010a
Kai Toedter <kai@toedter.com>
2020-10-24 12:07:59 +0200

    chore: optimize imports

--- lib/src/main/java/com/toedter/spring/hateoas/jsonapi/JsonApiTypeForClass.java
+++ lib/src/main/java/com/toedter/spring/hateoas/jsonapi/JsonApiTypeForClass.java
@@ -19,7 +19,7 @@ package com.toedter.spring.hateoas.jsonapi;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
