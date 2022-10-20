97745ed669ff196f10efed0b86e4519159e792d0
toedter <toedter@users.noreply.github.com>
2022-10-20 08:21:39 +0000

    deploy: 483410c98d848bb96b711dec0ecefed473de72a7

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.toedter.spring.hateoas.jsonapi"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.toedter.spring.hateoas.jsonapi"}];updateSearchResults();
